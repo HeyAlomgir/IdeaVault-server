b54d7a2b13b61e73e2af13b84325ae2be2300aa5
HeyAlomgir <alomgirhosssain71@gmail.com>
2026-05-21 11:05:44 +0600

    idya add to Databade

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
@@ -9,6 +10,9 @@ const uri =process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,14 +25,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+   await client.connect();
+
+   const db = client.db("idyavalut");
+   const idyavalutCollection = db.collection("idyas");
+
+   app.post('/idya',async (req,res)=>{
+    const idyaData = req.body;
+    console.log(idyaData);
+    const result = await idyavalutCollection.insertOne(idyaData);
+    res.json(result);
+   })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
